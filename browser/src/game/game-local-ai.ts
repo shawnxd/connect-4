16500ec9fa9741057a967ddb3870f05275acb595
shawnxd <shawnxdong@gmail.com>
2025-06-20 17:39:14 -0700

    Deploy Connect Four game for GitHub Pages

--- browser/src/game/game-local-ai.ts
+++
@@ -1,8 +0,0 @@
-import { BoardPiece } from '@kenrick95/c4'
-import { PlayerAi } from '@kenrick95/c4'
-import { GameLocal, initGameLocal } from './game-local'
-
-class GameLocalAi extends GameLocal {}
-export function initGameLocalAi() {
-  initGameLocal(GameLocalAi, new PlayerAi(BoardPiece.PLAYER_2))
-}
